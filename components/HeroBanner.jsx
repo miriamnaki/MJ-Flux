a34b04b7cdcd9497cef9d7ab9cd485fd2731bb0b
miriam-best-practice <nakiyingim@gmail.com>
2022-05-07 22:55:47 -0700

    add hero banner component

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,31 @@
+import Link from 'next/link';
 import React from 'react'
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+      <div>
+        <p className='beats-solo'>SMALL TEXT</p>
+
+        <h3>MID TEXT</h3>
+
+        <img src='' alt='headphones' className='hero-banner-image'/>
+
+        <div>
+          <Link href='/product/id'> 
+          <button>BUTTON</button>
+          </Link>
+
+          <div className='desc'>
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+
+
+      </div>
+
+    </div>
   )
 }
 
