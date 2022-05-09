9ffa5eb007db82a6fc9083425be9f1bf520d641e
miriam-best-practice <nakiyingim@gmail.com>
2022-05-09 14:33:57 -0700

    add products dynamically

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const Product = () => {
+// ger url for the image
+import { urlFor } from '../lib/client';
+import product from '../sanity_myeshop/schemas/product';
+
+const Product = ({product: {image, name, slug, price}}) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className='product-card'>
+          <img src={urlFor(image && image[0])}
+          width={250}
+          height={250}
+          className={product-image}
+          />
+
+          <p className='product-name'>{name}</p>
+          <p className='product-price'>${price}</p>
+
+        </div>
+      </Link>
+    </div>
   )
 }
 
