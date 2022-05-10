adeec725de5957ead296a0790f54dbeedb7e6dd1
miriam-best-practice <nakiyingim@gmail.com>
2022-05-10 07:52:41 -0700

    add product details page

---
+++ pages/product/[slug].js
@@ -0,0 +1,114 @@
+// square brackets mean the product is gonna be dynamic
+
+import React from 'react';
+import { client, urlFor } from '../../lib/client';
+import {AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from 'react-icons/ai';
+
+const ProductDetails = ({ product, products}) => {
+console.log(product)
+  const {image, name, details, price} = product;
+  return (
+    <div>
+      <div className='product-detail-container'>
+        <div>
+          <div className='image-container'>
+            <img src={urlFor(image && image[0])} className='product-detail-image'/>
+
+          </div>
+
+          {/* image carousel */}
+          {/* <div className='small-images-container'>
+            {image?.map((item, i) => (
+              <img
+              key={i}
+               src={urlFor(item)}
+               className=''
+               onMouseEnter=''
+               />
+            ))}
+
+          </div> */}
+
+
+        </div>
+        <div className='product-details-desc'>
+          <h1>{name}</h1>
+          <div className='reviews'>
+            <div>
+              <AiFillStar/>
+              <AiFillStar/>
+              <AiFillStar/>
+              <AiFillStar/>
+              <AiOutlineStar/>
+            </div>
+
+            <p>(20)</p>
+
+          </div>
+
+          <h4>Details</h4>
+          <p>{details}</p>
+          <p className='price'>${price}</p>
+          <div className='quantity'>
+            <h3>Quantity:</h3>
+            <p className='quantity-desc'>
+              <span className='minus' onClick=""><AiOutlineMinus/></span>
+              <span className='num' onClick="">0</span>
+              <span className='plus' onClick=""><AiOutlinePlus/></span>
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+// set static paths
+export const getStaticPaths = async () => {
+  // get the query
+  // get all products but just the specifix product
+  // get current property of a specific slug
+  const query = `*[_type == "product"] {    
+    slug {
+      current
+    }
+  }
+  `
+  const products = await client.fetch(query);
+
+  // return a param object
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current
+    }
+  }));
+
+  // return an object with a path and callback
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+
+}
+
+export const getStaticProps = async ({params: {slug}}) => {
+  // form sanity query for both product and products
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const product = await client.fetch(query);
+
+  
+  const productsQuery = '*[_type == "product"]';
+  const products = await client.fetch(productsQuery);
+
+  
+ console.log(product)
+  // what getServerSide props returns gets populated in the homapage fields
+  return {
+    props: {products, product}
+  }
+}
+
+export default ProductDetails
