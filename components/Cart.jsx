d8d924ba1190b3ef847159b379456034d8bd28a9
miriam-best-practice <nakiyingim@gmail.com>
2022-05-10 16:17:55 -0700

    add get stripe api

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -10,6 +10,10 @@ const Cart = () => {
   const cartRef = useRef();
   const {totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, removeFromCart} = useStateContext();
 
+  const handleCheckout = () => {
+    
+  }
+
   return (
     <div className='cart-wrapper' ref={cartRef}>
       <div className='cart-container'>
@@ -71,7 +75,7 @@ const Cart = () => {
             </div>
 
             <div className='btn-container'>
-              <button type='button' className='btn' onClick="">Pay with Stripe</button>
+              <button type='button' className='btn' onClick={handleCheckout}>Pay with Stripe</button>
               
               
             </div>
