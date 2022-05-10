c4d261c189dd8bcc01fdab191d46907781c5f3d9
miriam-best-practice <nakiyingim@gmail.com>
2022-05-10 09:23:28 -0700

    fix small cimage carousel

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,34 +1,45 @@
 // square brackets mean the product is gonna be dynamic
 
-import React from 'react';
+import React, {useState} from 'react';
 import { client, urlFor } from '../../lib/client';
 import {AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from 'react-icons/ai';
 import Product from '../../components/Product';
 
+
+
 const ProductDetails = ({ product, products}) => {
-console.log(product)
+// console.log(product)
   const {image, name, details, price} = product;
+  console.log(image)
+  console.log(name)
+
+  // useState hook
+  const [index, setIndex] = useState(0);
   return (
     <div>
       <div className='product-detail-container'>
         <div>
           <div className='image-container'>
-            <img src={urlFor(image && image[0])} className='product-detail-image'/>
+            <img src={urlFor(image && image[index])} className='product-detail-image'/>
 
           </div>
 
-          {/* image carousel */}
-          {/* <div className='small-images-container'>
+          {/* small image carousel */}
+          <div className='small-images-container'>
             {image?.map((item, i) => (
+              
               <img
+             
               key={i}
                src={urlFor(item)}
-               className=''
-               onMouseEnter=''
+               className={i === index ? 'small-image selected-image' : 'small-image'}
+               onMouseEnter={() => setIndex(i)}
                />
+              
+              
             ))}
 
-          </div> */}
+          </div>
 
 
         </div>
