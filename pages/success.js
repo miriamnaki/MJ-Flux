03e2e17a38f8a8d48d4c1d9d0b6b08b6a9365778
miriam-best-practice <nakiyingim@gmail.com>
2022-05-16 15:35:07 -0700

    add confetti fireworks to success page

--- pages/success.js
+++ pages/success.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import Link from 'next/link';
 import {BsBagCheckFill} from 'react-icons/bs';
 import { useStateContext } from '../context/StateContext';
+import { runFireworks } from '../lib/utils';
 
 const Success = () => {
   const {setCartItems, setTotalPrice, setTotalQuantities} = useStateContext();
@@ -11,6 +12,7 @@ const Success = () => {
     setCartItems([]);
     setTotalPrice(0);
     setTotalQuantities(0)
+    runFireworks()
   }, [])
 
 
