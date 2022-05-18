34ccd023c72eca75db58f36ea44fc70e6d11c835
miriam-best-practice <nakiyingim@gmail.com>
2022-05-17 23:03:37 -0700

    add animation and confetti for landing page

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,17 +1,24 @@
 import Link from 'next/link';
 import React from 'react';
 import { urlFor } from '../lib/client';
+import { useEffect } from 'react';
+import { runSchoolPride } from '../lib/utils2';
 
 const HeroBanner = ({heroBanner}) => {
+
+  useEffect(() => {
+    runSchoolPride()
+  },[])
+
   return (
     <div className='hero-banner-container'>
-      <div>
+      <div className='hero-banner-inner'>
         <p className='beats-solo'>{heroBanner.smallText}</p>
 
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
 
-        <img src={urlFor(heroBanner.image)} alt='headphones' className='hero-banner-image'/>
+        <img src={urlFor(heroBanner.image)} alt='landing-image' className='hero-banner-image'/>
 
         <div>
           <Link href={`/product/${heroBanner.product}`}> 
--- components/Product.jsx
+++ components/Product.jsx
@@ -11,12 +11,12 @@ const Product = ({product: {image, name, slug, price}}) => {
       <Link href={`/product/${slug.current}`}>
         <div className='product-card'>
           <img src={urlFor(image && image[0])}
-          width={250}
-          height={250}
-          className={product-image}
+          width={300}
+          height={300}
+          className='product-image'
           />
-
           <p className='product-name'>{name}</p>
+
           <p className='product-price'>${price}</p>
 
         </div>
